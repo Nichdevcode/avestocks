edb39c264baaaf5fbb9cdd8dd081c615d59e1a60
nichdevcode <duadeinicholas14@gmail.com>
2023-08-31 02:55:55 +0100

    mailer

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -46,26 +46,27 @@ export const mailer =  async ({ subject, message, to }: { subject: string, messa
 
 export const mailerAsync =  async ({ subject, message, to }: { subject: string, message: string, to: string }) => {
   // try {
-      const transporter = nodemailer.createTransport({
-        service: 'zoho',
-        // host: "smtp.zoho.com",
-        host: 'smtp.zoho.com',
-        // host: 'smtppro.zoho.com',
-        authMethod: "LOGIN",
-        // port: 587,
-        // secure: false,
-        port: 465,
-        secure: true,
-        auth: {
-          // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-          user: "avestocks@avestocks.com",
-          pass: "Avestocks1@#",
-        },
-      });
+    const transporter = nodemailer.createTransport({
+      service: 'zoho',
+      // host: "smtp.zoho.com",
+      host: 'smtp.zoho.com',
+      // host: 'smtppro.zoho.com',
+      authMethod: "LOGIN",
+      // port: 587,
+      // secure: false,
+      port: 465,
+      secure: true,
+      auth: {
+        // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+        user: "support@avestocks.com",
+        pass: "Avestocks1@#",
+      },
+    });
+
 
 
       const mailOptions = {
-        from: "avestocks@avestocks.com",
+        from: "support@avestocks.com",
         to,
         // to: "nicholasduadei14@gmail.com",
         subject: subject || "Avestocks",
